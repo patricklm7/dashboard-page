97e3e8d129e607d1ed2768923350e4661f7911c4
patricklm7 <patricklm7@gmail.com>
2023-09-19 17:55:37 -0300

    adicicao script

---
+++ script.js
@@ -0,0 +1,10 @@
+const dashboardPage = document.getElementById('container')
+const saleInfo = document.getElementById('sale-info')
+const mouthRevenue = document.getElementById('mouth-revenue')
+const totalProfit = document.getElementById('total-profit')
+const mouthForm = document.getElementById('meses')
+
+const mouth = mouthForm.value;
+console.log(mouth);
+
+saleInfo.textContent = '';
